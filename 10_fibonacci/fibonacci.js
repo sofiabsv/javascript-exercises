f1aa42d1abb646f8e325bdec596d6bc6c75b8c76
Sofia Berdichevska <sofiaxberdichevska@gmail.com>
2024-06-15 18:07:29 +0200

    Solved Task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (count) {
+  if (count < 0) {
+    return "OOPS";
+  } else if (count == 0) {
+    return 0;
+  }
+  let first = 1;
+  let second = 0;
 
+  for (let i = 2; i <= count; i++) {
+    let newElement = first + second;
+    second = first;
+    first = newElement;
+  }
+  return first;
 };
 
 // Do not edit below this line
