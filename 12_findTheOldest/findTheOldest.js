5cfb0f0e07b401db63f51d4d92f6f48a0bad5842
Sofia Berdichevska <sofiaxberdichevska@gmail.com>
2024-06-15 19:08:10 +0200

    Solved Task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  function getAge(birthYear, deathYear) {
+    if (!deathYear) {
+      deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear;
+  }
 
+  let newArray = arr.map((arr) => getAge(arr.yearOfBirth, arr.yearOfDeath));
+
+  let oldest = 0;
+  for (let i = 1; i < newArray.length; i++) {
+    if (newArray[oldest] < newArray[i]) {
+      oldest = i;
+    }
+  }
+  return arr[oldest];
 };
 
 // Do not edit below this line
