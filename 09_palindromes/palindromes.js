9cd27cb611277409e705c3da83939bb9da02d415
Sofia Berdichevska <sofiaxberdichevska@gmail.com>
2024-06-15 17:46:14 +0200

    Solved Task 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanum = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const newString = str
+    .toLowerCase()
+    .split("")
+    .filter((item) => alphanum.includes(item))
+    .join("");
+
+  const reversedString = newString.split("").reverse().join("");
+
+  return reversedString === newString;
 };
 
 // Do not edit below this line
